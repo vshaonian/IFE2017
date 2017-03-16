0718a76e1aef64134d92e74de90adc3c7ac4dfba
vshaonian <1251602300@qq.com>
2017-03-16 23:24:51 +0800

    完成ECharts配置

--- ECharts/task4/k.js
+++ ECharts/task4/k.js
@@ -2,13 +2,13 @@ function splitData(rawData) {
     var categoryData = [];
     var values = [];
     var volumns = [];
-    for(var i = 0; i < rawData.length; i++) {
-        var dateString = rawData[i].Data;
+    for (var i = 0; i < rawData.length; i++) {
+        var dateString = rawData[i].Date;
         var pattern = /(\d{4})(\d{2})(\d{2})/;
         var dateNewType = dateString.replace(pattern, '$1-$2-$3');
         categoryData.push(dateNewType);
 
-        values.push([rawData[i].Open,rawData[i].Close,rawData[i].Low,rawData[i].High]);
+        values.push([rawData[i].Open, rawData[i].Close, rawData[i].Low, rawData[i].High]);
 
         volumns.push(rawData[i].Volume);
     }
@@ -21,29 +21,180 @@ function splitData(rawData) {
 
 var data0 = splitData(data);
 
-console.log(data0);
+
 
 var myCharts = echarts.init(document.getElementById('main'));
 
 var option = {
-    	title: {
-            text: 'K线图'
+    title: {
+        text: 'K线图',
+        left: 0
+    },
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+            type: 'line'
+        }
+    },
+    legend: {
+        data: ['日K', 'MA5', 'MA10', 'MA20', 'MA30']
+    },
+    grid: {
+        left: '10%',
+        right: '10%',
+        bottom: '15%'
+    },
+    xAxis: {
+        type: 'category',
+        data: data0.categoryData,
+        scale: true,
+        boundaryGap: false,
+        axisLine: { onZero: false },
+        splitLine: { show: false },
+        splitNumber: 20,
+        min: 'dataMin',
+        max: 'dataMax'
+    },
+    yAxis: {
+        scale: true,
+        splitArea: {
+            show: true
+        }
+    },
+    dataZoom: [
+        {
+            type: 'inside',
+            start: 50,
+            end: 100
         },
-        tooltip: {},
-        legend: {
-            data:['利润','销量']
+        {
+            show: true,
+            type: 'slider',
+            y: '90%',
+            start: 50,
+            end: 100
+        }
+    ],
+    series: [
+        {
+            name: '日K',
+            type: 'candlestick',
+            data: data0.values,
+            markPoint: {
+                lable: {
+                    normal: {
+                        formatter: function (param) {
+                            return param != null ? Math.round(param.value) : '';
+                        }
+                    }
+                },
+                data: [
+                    {
+                        name: 'XX标点',
+                        coord: ['2013/5/31', 2300],
+                        value: 2300,
+                        itemStyle: {
+                            normal: { color: 'rgb(41,60,85)' }
+                        }
+                    },
+                    {
+                        name: 'highest value',
+                        type: 'max',
+                        valueDim: 'highest'
+                    },
+                    {
+                        name: 'lowest value',
+                        type: 'min',
+                        valueDim: 'lowest'
+                    },
+                    {
+                        name: 'average value on close',
+                        type: 'average',
+                        valueDim: 'close'
+                    }
+                ],
+                tooltip: {
+                    formatter: function (param) {
+                        return param.name + '<br>' + (param.data.coord || '');
+                    }
+                }
+            },
+            markLine: {
+                symbol: ['none', 'none'],
+                data: [
+                    [
+                        {
+                            name: 'from lowest to highest',
+                            type: 'min',
+                            valueDim: 'lowest',
+                            symbol: 'circle',
+                            symbolSize: 10,
+                            label: {
+                                normal: { show: false },
+                                emphasis: { show: false }
+                            }
+                        },
+                        {
+                            type: 'max',
+                            valueDim: 'highest',
+                            symbol: 'circle',
+                            symbolSize: 10,
+                            label: {
+                                normal: { show: false },
+                                emphasis: { show: false }
+                            }
+                        }
+                    ],
+                    {
+                        name: 'min line on close',
+                        type: 'min',
+                        valueDim: 'close'
+                    },
+                    {
+                        name: 'max line on close',
+                        type: 'max',
+                        valueDim: 'close'
+                    }
+                ]
+            }
         },
-        xAxis: {},
-        yAxis: {},
-        series: [
-	        {
-	        	name: '利润',
-	        	type: 'candlestick',
-	        	data: [10,20,12,22]
-	        }
-        ],
-        // 系列颜色
-        color: ["green",'blue']
-    };
+        {
+            name: 'MA5',
+            type: 'line',
+            data: calculateMA(5),
+            smooth: true,
+            lineStyle: {
+                normal: { opacity: 0.5 }
+            }
+        },
+        {
+            name: 'MA10',
+            type: 'line',
+            data: calculateMA(10),
+            smooth: true,
+            lineStyle: {
+                normal: { opacity: 0.5 }
+            }
+        },
+        {
+            name: 'MA20',
+            type: 'line',
+            data: calculateMA(20),
+            smooth: true,
+            lineStyle: {
+                normal: { opacity: 0.5 }
+            }
+        },
+        {
+            name: 'MA30',
+            type: 'line',
+            data: calculateMA(30),
+            smooth: true,
+            lineStyle: {
+                normal: { opacity: 0.5 }
+            }
+        }
+    ]
+};
 
-    myCharts.setOption(option);
+myCharts.setOption(option);

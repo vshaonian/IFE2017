298c852bcf4c5a67d0222548aad49a6cd7ee02d8
vshaonian <1251602300@qq.com>
2017-03-17 17:04:33 +0800

    完成K线图

--- ECharts/task4/k.js
+++ ECharts/task4/k.js
@@ -2,7 +2,7 @@ function splitData(rawData) {
     var categoryData = [];
     var values = [];
     var volumns = [];
-    for (var i = 0; i < rawData.length; i++) {
+    for (var i = 0; i < 240; i++) {
         var dateString = rawData[i].Date;
         var pattern = /(\d{4})(\d{2})(\d{2})/;
         var dateNewType = dateString.replace(pattern, '$1-$2-$3');
@@ -21,56 +21,107 @@ function splitData(rawData) {
 
 var data0 = splitData(data);
 
+// function calculateMA(dayCount) {
+//     var result = [];
+//     for (var i = 0; i < data0.values.length; i++) {
+//         if (i < dayCount) {
+//             result.push('-');
+//             continue;
+//         }
+//         var sum = 0;
+//         for (var j = 0; j < dayCount; j++) {
+//             sum += data0.values[i - j][1];
+//         }
+//         result.push(sum / dayCount);
+//     }
+//     return result;
+// }
 
+function calculateMA(dayCount){
+	var result = [];
+	for(var i = dayCount-1;i<data0.values.length;i++){
+		result[i] = 0;
+		for(var j = 0;j<dayCount;j++){
+			result[i] += parseFloat(data[i-j][1]);
+		}
+		result[i] /= dayCount;
+		result[i] = result[i].toFixed(2);
+	};
+	return result;
+}
 
 var myCharts = echarts.init(document.getElementById('main'));
 
 var option = {
     title: {
-        text: 'K线图',
+        text: 'Ticker: A',
         left: 0
     },
     tooltip: {
+        // 坐标轴触发
         trigger: 'axis',
+        // 坐标轴直线指示器
         axisPointer: {
-            type: 'line'
+            type: 'cross'
         }
     },
     legend: {
         data: ['日K', 'MA5', 'MA10', 'MA20', 'MA30']
     },
-    grid: {
-        left: '10%',
-        right: '10%',
-        bottom: '15%'
-    },
-    xAxis: {
-        type: 'category',
-        data: data0.categoryData,
-        scale: true,
-        boundaryGap: false,
-        axisLine: { onZero: false },
-        splitLine: { show: false },
-        splitNumber: 20,
-        min: 'dataMin',
-        max: 'dataMax'
-    },
-    yAxis: {
-        scale: true,
-        splitArea: {
-            show: true
+    // 网格
+    grid: [
+        {
+            bottom: '40%'
+        },
+        {
+            top: '70%',
+            bottom: '14%'
         }
-    },
+    ],
+    xAxis: [
+        {
+            // 类目轴
+            type: 'category',
+            // 类目数据
+            data: data0.categoryData,
+            scale: true,
+            // 坐标轴刻度最小值
+            min: 'dataMin',
+            // 坐标轴刻度最大值
+            max: 'dataMax'
+        },
+        {
+            type: 'category',
+            gridIndex: 1,
+            data: data0.categoryData
+        }
+    ],
+    yAxis: [
+        {
+            scale: true,
+            splitArea: {
+                show: true
+            }
+        },
+        {
+            scale: true,
+            gridIndex: 1
+        }
+    ],
+    // 区域缩放
     dataZoom: [
+        // 内置型数据区域缩放
         {
             type: 'inside',
             start: 50,
             end: 100
         },
+        // 滑动条型数据区域缩放
         {
             show: true,
             type: 'slider',
             y: '90%',
+            // 数据窗口范围的起始百分比
             start: 50,
             end: 100
         }
@@ -79,88 +130,20 @@ var option = {
         {
             name: '日K',
             type: 'candlestick',
-            data: data0.values,
-            markPoint: {
-                lable: {
-                    normal: {
-                        formatter: function (param) {
-                            return param != null ? Math.round(param.value) : '';
-                        }
-                    }
-                },
-                data: [
-                    {
-                        name: 'XX标点',
-                        coord: ['2013/5/31', 2300],
-                        value: 2300,
-                        itemStyle: {
-                            normal: { color: 'rgb(41,60,85)' }
-                        }
-                    },
-                    {
-                        name: 'highest value',
-                        type: 'max',
-                        valueDim: 'highest'
-                    },
-                    {
-                        name: 'lowest value',
-                        type: 'min',
-                        valueDim: 'lowest'
-                    },
-                    {
-                        name: 'average value on close',
-                        type: 'average',
-                        valueDim: 'close'
-                    }
-                ],
-                tooltip: {
-                    formatter: function (param) {
-                        return param.name + '<br>' + (param.data.coord || '');
-                    }
-                }
-            },
-            markLine: {
-                symbol: ['none', 'none'],
-                data: [
-                    [
-                        {
-                            name: 'from lowest to highest',
-                            type: 'min',
-                            valueDim: 'lowest',
-                            symbol: 'circle',
-                            symbolSize: 10,
-                            label: {
-                                normal: { show: false },
-                                emphasis: { show: false }
-                            }
-                        },
-                        {
-                            type: 'max',
-                            valueDim: 'highest',
-                            symbol: 'circle',
-                            symbolSize: 10,
-                            label: {
-                                normal: { show: false },
-                                emphasis: { show: false }
-                            }
-                        }
-                    ],
-                    {
-                        name: 'min line on close',
-                        type: 'min',
-                        valueDim: 'close'
-                    },
-                    {
-                        name: 'max line on close',
-                        type: 'max',
-                        valueDim: 'close'
-                    }
-                ]
-            }
+            data: data0.values
+        },
+        {
+            name: '成交量',
+            type: 'bar',
+            xAxisIndex: 1,
+            yAxisIndex: 1,
+            data: data0.volume
         },
         {
             name: 'MA5',
             type: 'line',
+            xAxisIndex: 0,
+            yAxisIndex: 0,
             data: calculateMA(5),
             smooth: true,
             lineStyle: {
@@ -170,6 +153,8 @@ var option = {
         {
             name: 'MA10',
             type: 'line',
+            xAxisIndex: 0,
+            yAxisIndex: 0,
             data: calculateMA(10),
             smooth: true,
             lineStyle: {
@@ -179,6 +164,8 @@ var option = {
         {
             name: 'MA20',
             type: 'line',
+            xAxisIndex: 0,
+            yAxisIndex: 0,
             data: calculateMA(20),
             smooth: true,
             lineStyle: {
@@ -188,6 +175,8 @@ var option = {
         {
             name: 'MA30',
             type: 'line',
+            xAxisIndex: 0,
+            yAxisIndex: 0,
             data: calculateMA(30),
             smooth: true,
             lineStyle: {
